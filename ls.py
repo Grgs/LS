92ad652784c26a3b5dec80db1769b1d916292156
Michael Guirguis <guirguis@michaelguirguis.com>
2019-01-06 14:15:21 -0500

    sort dot files

--- ls.py
+++ ls.py
@@ -2,22 +2,14 @@
 """display directory files and folders"""
 import subprocess
 import sys
-from itertools import *
+from itertools import chain, compress, repeat
+from typing import List
 
 import regex as re
 
 from guirguis.filter import filter_dict
 
 COLOR_STOP = "[0m"
-
-
-# def _safe_max(input_blob):
-#     l = -1
-#     try:
-#         l = len(input_blob)
-#     except:
-#         l = 0
-#     return l
 
 
 def _combine_cleaned(clean_fun, text_list):
@@ -44,31 +36,37 @@ def _other_file_test(line: str) -> bool:
     return len(line) > 0
 
 
-def _filter_start(line: str, filter_select):
-    return compress(re.split(r"\s+", line, maxsplit=3), chain(filter_select, repeat(1)))
+def _split_start(line: str) -> List[str]:
+    return re.split(r"\s+", line, maxsplit=3)
 
 
-def _clean_dirs(line: str):
-    return " ".join(_filter_start(line, [0, 0, 0])) + COLOR_STOP
+def _compress_start(chuncks: List[str], filter_select: List[int]):
+    return compress(chuncks, chain(filter_select, repeat(1)))
 
 
-def _clean_files_in_mnt(line: str) -> str:
-    output_0, output_1, output_2 = _filter_start(line, [1, 0])
-    return "{} {:>6} {}".format(output_0[1:], output_1, output_2) + COLOR_STOP
+def _split_compress_start(line: str, filter_select: List[int]):
+    return _compress_start(_split_start(line), filter_select)
+
+
+def _clean_dirs(line: str) -> str:
+    return _split_start(line)[-1] + COLOR_STOP
+
+
+def _clean_regular_files(line: str) -> str:
+    return "{} {:>6} {}".format(*_split_compress_start(line, [1, 0]))[1:] + COLOR_STOP
 
 
 def _clean_other_files(line: str) -> str:
-    output_0, output_1 = _filter_start(line, [1, 0, 0])
+    output_0, output_1 = _split_compress_start(line, [1, 0, 0])
     return "{} {}".format(output_0[0], output_1) + COLOR_STOP
 
 
-def _clean_files_regular(line: str) -> str:
-    output_0, output_1 = _filter_start(line, [0, 0])
-    return "{:>6} {}".format(output_0, output_1) + COLOR_STOP
+def _clean_mnt_files(line: str) -> str:
+    return "{:>6} {}".format(*_split_compress_start(line, [0, 0])) + COLOR_STOP
 
 
 def _clean_files_tmp(line: str) -> str:
-    return re.sub(r"\x1b\[0m|\x1b\[01;\d+m", "", _clean_files_regular(line))
+    return re.sub(r"\x1b\[0m|\x1b\[01;\d+m", "", _clean_mnt_files(line))
 
 
 def _separate_header_from_text(text: str):
@@ -85,8 +83,8 @@ def _file_cleaner():
         .stdout.decode("utf-8")
         .startswith("/mnt/")
     ):
-        return _clean_files_regular
-    return _clean_files_in_mnt
+        return _clean_mnt_files
+    return _clean_regular_files
 
 
 def _get_shell_args():
@@ -102,6 +100,14 @@ def _get_shell_text(shell_args) -> str:
 def _set_max_len(fdata):
     for k, f in fdata.items():
         fdata[k]["max_len"] = max(f["data"], key=len)
+
+
+def _is_hidden(text: str):
+    return bool(re.search(r"\s(\x1b\[\d+;\d+m)?\.", text, flags=re.V1))
+
+
+def _sort_dot(lines):
+    return sorted(lines, key=_is_hidden, reverse=True)
 
 
 FDATA = {
@@ -143,9 +149,14 @@ def main():
     head, text = _separate_header_from_text(text)
     filter_dict(text.splitlines(), FDATA, LINE_FILTER_ORDER, LINE_FILTER_ORDER_FALLBACK)
     for fname, fval in FDATA.items():
-        FDATA[fname]["data"] = _combine_cleaned(fval["cleaner"], fval["data"])
+        FDATA[fname]["data"] = _sort_dot(
+            _combine_cleaned(fval["cleaner"], fval["data"])
+        )
+        # FDATA[fname]["data"] =   sorted()      FDATA[fname]["data"]
         # FDATA[fname]["max_len"] = max(map(len, FDATA[fname]["data"]), default=0)
+        # sorted(FDATA[x]["data"], key=lambda s: " ." in s, reverse=True)
     output_list = chain(*[FDATA[x]["data"] for x in LINE_OUTPUT_ORDER])
+    # output_list = chain(*[_sort_dot(FDATA[x]["data"]) for x in LINE_OUTPUT_ORDER])
     print("\n".join(output_list))
     print(head)
 
