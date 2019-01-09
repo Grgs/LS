cc9fffd45c9bcf36a65b18b95620165e0ec6afe4
Michael Guirguis <guirguis@michaelguirguis.com>
2019-01-08 19:39:25 -0500

    minor format

--- ls.cpp
+++ ls.cpp
@@ -9,23 +9,23 @@
 
 std::string exec(const char *cmd)
 {
-    std::array<char, 128> buffer;
-    std::string result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-    if (!pipe)
-    {
-        throw std::runtime_error("popen() failed!");
-    }
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
-    {
-        result += buffer.data();
-    }
-    return result;
+  std::array<char, 128> buffer;
+  std::string result;
+  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+  if (!pipe)
+  {
+    throw std::runtime_error("popen() failed!");
+  }
+  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
+  {
+    result += buffer.data();
+  }
+  return result;
 }
 
 int main()
 {
-    auto text = exec("ls -gGASh --color=always");
-    std::cout << text;
-    return 0;
+  auto text = exec("ls -gGASh --color=always");
+  std::cout << text;
+  return 0;
 }
