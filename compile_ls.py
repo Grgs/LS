6a656e99b3aab9b072b7c87445d190676f19b316
Michael Guirguis <guirguis@michaelguirguis.com>
2019-01-18 18:34:52 -0500

    minor: quotes

--- compile_ls.py
+++ compile_ls.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""compile ls.py"""
+'''compile ls.py'''
 import py_compile
 import compileall
 
 compileall.compile_dir('./', force=True)
-py_compile.compile("ls.py", cfile="cls.pyc", optimize=2)
+py_compile.compile('ls.py', cfile='cls.pyc', optimize=2)
