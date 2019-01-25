a51325bac86750131b63028d5dcecb275f65df62
Michael Guirguis <guirguis@michaelguirguis.com>
2019-01-25 08:49:43 -0500

    fixed time again

--- LsNums.py
+++ LsNums.py
@@ -1,10 +1,10 @@
 from datetime import datetime as dt
-from datetime import timedelta
+# import regex as re
 
 import bitmath
 
 
-class FNums:
+class FField:
 
     def __init__(self, value):
         self.value = value
@@ -39,35 +39,51 @@ class FNums:
     def _str(self) -> str:
         return str(self.value)
 
+# class FName(FField):
 
-class FSize(FNums):
+#     def __init__(self, value):
+#         super().__init__(value)
+#         if len(self.value) < 2:
+#             self._first_char = self.value
+#             self._pieces = []
+#             self.base = self.value
+#             self.extentions = []
+#         else:
+#             self._first_char = self.value[0]
+#             self._pieces = self.value[1:].split('.')
+#             self.base = self._first_char + self._pieces[0]
+#             self.extentions = []
+#             if len(self._pieces) > 1:
+#                 self.extentions = self._pieces[1:]
+
+#     def _str(self) -> str:
+#         return self.value
+
+class FSize(FField):
 
     def _str(self) -> str:
         value = bitmath.Byte(self.value).best_prefix()
         return '{0:>6.1f}{1}'.format(value.value, value.unit[0])
 
 
-class FTime(FNums):
+class FTime(FField):
 
     def __init__(self, value, current_time=dt.now()):
         self.current_time = current_time
         self.value_date = dt.utcfromtimestamp(value)
         super().__init__(value)
 
-
     def _str(self) -> str:
         if self.value_date.year != self.current_time.year:
             return '{:%Y-%m}'.format(self.value_date)
         if self.value_date.month != self.current_time.month:
             return '{:%m-%d}'.format(self.value_date)
-        diffdate = self.current_time - self.value_date
-        if self.value_date.day != self.current_time.day:
-            return '{0:>2}d {1:>2}h'.format(
-                abs(diffdate.days), (diffdate - timedelta(days=diffdate.days))
-                // timedelta(hours=1))
-        if self.value_date.hour < self.current_time.hour:
-            return '{0:>2}h {1:>2}m'.format(
-                int(diffdate / timedelta(hours=1)),
-                diffdate.seconds % (60 * 60) // 60)
-        return '{0:>2}m {1:>2}s'.format(diffdate.seconds % (60 * 60) // 60,
-                                        diffdate.seconds % 60)
+        diffdate = abs(self.current_time - self.value_date)
+        diffhours = (diffdate.seconds // 3600) % 24
+        if diffdate.days != 0:
+            return '{0:>2}d {1:>2}h'.format(diffdate.days, diffhours)
+        diffminutes = (diffdate.seconds // 60) % 60
+        if diffhours != 0:
+            return '{0:>2}h {1:>2}m'.format(diffhours, diffminutes)
+        diffseconds = diffdate.seconds % 60
+        return '{0:>2}m {1:>2}s'.format(diffminutes, diffseconds)
